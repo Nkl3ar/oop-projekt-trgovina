0dd91c151d4a942a7f8f6e71fe77a0714900b2e5
Filip <ftutic@vub.hr>
2023-01-13 21:42:57 +0100

    formatted Proizvod

--- main.cpp
+++ main.cpp
@@ -1,43 +1,49 @@
 #include <iostream>
-using std::string;
 using std::cout, std::cin, std::endl;
+using std::string;
 
-class Proizvod{
-    private:
-        string ime;
-        float cijena;
-    public:
-        Proizvod(string ime, float cijena) : ime(ime), cijena(cijena) {}
-        Proizvod(string ime) {
-            this-> ime = ime;
-            cijena = 0;
-        }
-        Proizvod(): Proizvod("",0){}
-        
-        string getIme(){
-            return ime;
-        }
-        void setIme(string ime){
-            this->ime = ime;
-        }
+class Proizvod
+{
+private:
+    string ime;
+    float cijena;
 
-        float getCijena()
-        {
-            return cijena;
-        }
+public:
+    Proizvod(string ime, float cijena) : ime(ime), cijena(cijena) {}
 
-        void setCijena(float cijena)
-        {
-            this->cijena;
-        }
+    Proizvod(string ime, float cijena) : ime(ime), cijena(cijena) {}
+    Proizvod(string ime)
+    {
+        this->ime = ime;
+        cijena = 0;
+    }
+    Proizvod() : Proizvod("", 0) {}
 
+    string getIme()
+    {
+        return ime;
+    }
+    void setIme(string ime)
+    {
+        this->ime = ime;
+    }
+
+    float getCijena()
+    {
+        return cijena;
+    }
+
+    void setCijena(float cijena)
+    {
+        this->cijena;
+    }
 };
-
 
 int main(int argc, char const *argv[])
 {
-    cout << "Dobro došli u Zumkon" << endl << "Vaše pare su naše pare" << endl;
-    Proizvod p1("test",1);
+    cout << "Dobro došli u Zumkon" << endl
+         << "Vaše pare su naše pare" << endl;
+    Proizvod p1("test", 1);
     Proizvod p2("test2");
     Proizvod p3;
     cout << p1.getIme() << " " << p1.getCijena() << endl;
