cbdd68cc101e95e9ff45371e8f0cc708a13e728f
Filip <ftutic@vub.hr>
2023-01-13 23:46:55 +0100

    formatted

--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@ class Proizvod
 
 private:
     double validCijena(const float cijena)
-        {
-            if (cijena < 0)
-                return 0;
-            return cijena;
-        }
+    {
+        if (cijena < 0)
+            return 0;
+        return cijena;
+    }
 
 protected:
     string ime;
@@ -60,76 +60,78 @@ public:
     }
 };
 
-class Kamen : public Proizvod{
-    private:
-        float validTezina(const float tezina)
-        {
-            if(tezina<0)
-                return 0;
-            return tezina;
-        }
-    protected:
-        string tip;
-        float tezina;
-    public:
-        Kamen(string ime, float cijena, string tip, float tezina) : Proizvod(ime,cijena), tip(tip), tezina(validTezina(tezina)){}
-        const string getTip()
-        {
-            return tip;
-        }
-        void setTip(string tip)
-        {
-            this->tip = tip;
-        }
-        const float getTezina()
-        {
-            return tezina;
-        }
-        void setTezina(float tezina)
-        {
-            this->tezina=tezina;
-        }
-        void info()
-        {
-            Proizvod::info();
-            cout << "Tip: " << tip << endl;
-            cout << "Tezina: " << tezina << endl;
-        }
-        bool operator==(Kamen &k)
-        {
+class Kamen : public Proizvod
+{
+private:
+    float validTezina(const float tezina)
+    {
+        if (tezina < 0)
+            return 0;
+        return tezina;
+    }
+
+protected:
+    string tip;
+    float tezina;
+
+public:
+    Kamen(string ime, float cijena, string tip, float tezina) : Proizvod(ime, cijena), tip(tip), tezina(validTezina(tezina)) {}
+    const string getTip()
+    {
+        return tip;
+    }
+    void setTip(string tip)
+    {
+        this->tip = tip;
+    }
+    const float getTezina()
+    {
+        return tezina;
+    }
+    void setTezina(float tezina)
+    {
+        this->tezina = tezina;
+    }
+    void info()
+    {
+        Proizvod::info();
+        cout << "Tip: " << tip << endl;
+        cout << "Tezina: " << tezina << endl;
+    }
+    bool operator==(Kamen &k)
+    {
         if (cijena == k.getCijena())
             if (ime == k.getIme())
-                if(tip == k.getTip())
-                    if(tezina == k.getTezina())
+                if (tip == k.getTip())
+                    if (tezina == k.getTezina())
                         return true;
         return false;
-        }
-        
+    }
 };
 
 int main()
 {
     cout << "Dobro došli u Zumkon" << endl
-         << "Vaše pare su naše pare" << endl;/*
-    Proizvod p1("test", 1);
-    Proizvod p2("test2");
-    Proizvod p3;
-    cout << p1.getIme() << " " << p1.getCijena() << endl;
-    cout << p2.getIme() << " " << p2.getCijena() << endl;
-    cout << p3.getIme() << " " << p3.getCijena() << endl;
-    string unos;
-    cin >> unos;
-    p3.setIme(unos);
-    float cijena;
-    cin >> cijena;
-    p3.setCijena(cijena);
-    cout << p3.getIme() << " " << p3.getCijena() << endl;
-    bool result = p3 == p1;
-    cout << result;*/
+         << "Vaše pare su naše pare" << endl; /*
+     Proizvod p1("test", 1);
+     Proizvod p2("test2");
+     Proizvod p3;
+     cout << p1.getIme() << " " << p1.getCijena() << endl;
+     cout << p2.getIme() << " " << p2.getCijena() << endl;
+     cout << p3.getIme() << " " << p3.getCijena() << endl;
+     string unos;
+     cin >> unos;
+     p3.setIme(unos);
+     float cijena;
+     cin >> cijena;
+     p3.setCijena(cijena);
+     cout << p3.getIme() << " " << p3.getCijena() << endl;
+     bool result = p3 == p1;
+     cout << result;*/
 
-    Kamen k1 ("Test",25,"P",-1.1);
-    Kamen k2 ("Test",20,"P",-1.1);
-    Kamen k3 ("Test",25,"P",-1.1);
+    Kamen k1("Test", 25, "P", -1.1);
+    Kamen k2("Test", 20, "P", -1.1);
+    Kamen k3("Test", 25, "P", -1.1);
     k1.info();
     bool k1k2 = k1 == k2;
     cout << k1k2 << endl;
